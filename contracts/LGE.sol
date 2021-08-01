9d08e041a71b9dfdb67d1b5973a1cf9a618253a4
CarsonCase <carsoncase@pop-os.localdomain>
2021-08-01 15:02:52 -0700

    added LGE

--- contracts/LGE.sol
+++ contracts/LGE.sol
@@ -10,11 +10,12 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 * @title LGERC20. An ERC20 variant with Uniswap LGE features
 * @author Carson Case (carsonpcase@gmail.com) 
  */
-contract LGERC20 is Ownable{
+contract LGE is Ownable{
     /* =====Events=====*/
     event ethRaised(address,uint256);
 
     /* =====State Variables=====*/
+    bool public immutable isWhitelist;
     uint256 public immutable LGESupply;
     uint256 public totalETHContributed;
     uint256 public immutable endTime;
@@ -24,6 +25,7 @@ contract LGERC20 is Ownable{
 
     /*=====Data Structures=====*/
     mapping(address => uint256) public contributers;
+    mapping(address => bool) public whitelist;
     address[] public contributerList;
 
     /*=====Interfaces=====*/
@@ -31,14 +33,17 @@ contract LGERC20 is Ownable{
     
     /*=====Constructor=====*/
     constructor(
+    bool    _isWhielist,                    //->true if whitelist on who can contribute
     address _token,                         //->ERC20 token
     uint256 _LGESupply,                     //->ERC20 Tokens to be minted for LGE
     uint256 _devFee,                        //->ERC20 Tokens to be minted for Dev Fee                  
     address _UniswapV2Router02,             //->Address of Uniswap Router             
     address _LPTokenReceiver,               //->Address of the recipitent of Liquidity tokens      
-    uint256 _endTime)                       //->End time for LGE               
+    uint256 _endTime                        //->End time for LGE 
+    )              
     Ownable()
     {
+        isWhitelist = _isWhielist;
         UniswapV2Router02 = IUniswapV2Router02(_UniswapV2Router02);     //Initialize Uniswap router
         LGESupply = _LGESupply;                                         //Note tokens to be sent off in LGE
         token = IContraxMintableERC20(_token);
@@ -52,12 +57,26 @@ contract LGERC20 is Ownable{
     * @dev receive function is how you contribute. Can only do when LGE is on
      */
     receive() payable external{
+        if(isWhitelist){
+            require(whitelist[msg.sender], "You are not permitted to participate in this LGE");
+        }
         require(!_isOver());                                                        //Require LGE is still going on
         emit ethRaised(msg.sender,msg.value);
         totalETHContributed += msg.value;                                           //Increase total value of ETH raised
         contributerList.push(msg.sender);                                           //Push to list of contributers                                           
         contributers[msg.sender] = msg.value;                                       //Note how much was contributed
     }   
+
+    /**
+    * @dev function to add an array of addresses to the whitelist or remove them
+    * @param _setStatus should be true if the users are to be whitelisted, and false if the users are to be blacklisted
+     */
+    function updateWhitelist(address[] memory _list, bool _setStatus)external onlyOwner{
+        require(isWhitelist, "This is not a whitelist LGE");
+        for(uint i = 0; i < _list.length; i++){
+            whitelist[_list[i]] = _setStatus;
+        }
+    }
 
     /**
     * @dev sweep function removes tokens in contract and sends em to owner
--- contracts/SupportFactory.sol
+++ contracts/SupportFactory.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./MasterChef.sol";
 import "./TokenBar.sol";
+import "./LGE.sol";
 
 contract SupportFactory{
     /**
@@ -11,6 +12,7 @@ contract SupportFactory{
      */
     event NewChef(address newChefAddress);
     event NewBar(address newBarAddress);
+    event NewLGE(address newLGEAddress);
 
     address public immutable uniswapV2Router;
 
@@ -43,5 +45,18 @@ contract SupportFactory{
         TokenBar bar = new TokenBar(_token, uniswapV2Router);
         emit NewBar(address(bar));
         return(address(bar));
+    }
+
+    /**
+    * @dev builds an LGE contract
+    * @param _token the token to mint for the LGE, lge contract must be given mint permissions
+    * @param _supply is the supply to mint for liquidity
+    * @param _devFee is the dev fee to give the dev. Obviously.
+    * @param _endTime is the time the LGE is over
+     */
+    function buildLGE(address _token, uint _supply, uint _devFee, uint _endTime)external returns(address){
+        LGE lge = new LGE(false, _token, _supply, _devFee, uniswapV2Router, msg.sender, _endTime);
+        emit NewLGE(address(lge));
+        return(address(lge));
     }
 }
